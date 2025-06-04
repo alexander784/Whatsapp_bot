d059ef4c92401472285286ff69cc5b6dc97b7a6b
alexander784 <alexandernyaga1999@gmail.com>
2025-06-04 16:53:41 +0300

    .env file

--- whatsapp_chatbot/whatsapp_chatbot/settings.py
+++ whatsapp_chatbot/whatsapp_chatbot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+TWILIO_ACCOUNT_SID = config('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = config('TWILIO_AUTH_TOKEN')
+TWILIO_WHATSAPP_NUMBER = config('TWILIO_WHATSAPP_NUMBER')
 
 ROOT_URLCONF = 'whatsapp_chatbot.urls'
 
